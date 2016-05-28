b712fc598a31104c3e1c18fe4210524c247f1dc8
Victor Nițu <victor@thesponge.eu>
2016-05-28 22:31:47 +0300

    Add edge weight

--- addon/components/visjs-edge.js
+++ addon/components/visjs-edge.js
@@ -24,6 +24,19 @@ export default VisJsChild.extend({
     return `${this.get('from')}-${this.get('to')}`;
   }),
 
+  /**
+   * @public
+   *
+   * If set this defines edge's weight (thickness).
+   * @type {Int}
+   */
+  value: undefined,
+
+  valueChanged: Ember.observer('value', function() {
+    let container = this.get('containerLayer');
+    container.updateEdgeValue(this.get('eId'), this.get('value'));
+  }),
+
   arrowChanged: Ember.observer('arrows', function() {
     let container = this.get('containerLayer');
     container.updateEdgeArrow(this.get('eId'), this.get('arrows'));
