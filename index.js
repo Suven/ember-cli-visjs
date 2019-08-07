9aa610adb956780077753af01cb01f1b58a2bb84
Abhilash Murthy <abhilashmurthy92@gmail.com>
2019-08-07 18:19:59 +0800

    upgrade to make work with ember 3.5+

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-/* jshint node: true */
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-visjs',
+  name: require('./package').name,
 
   included: function(app) {
     app.import(app.bowerDirectory + '/vis/dist/vis.js');
